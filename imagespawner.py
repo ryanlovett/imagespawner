ac074c7a11276a4adcacd03ba401ad3ad8336c3f
ryanlovett <rylo@berkeley.edu>
2016-07-13 11:06:23 -0700

    Use jupyter coding style. Put real work in start().
    options_from_form() should only format data. The real work should be done in start().

--- imagespawner.py
+++ imagespawner.py
@@ -1,56 +1,83 @@
 from dockerspawner import DockerSpawner
 from traitlets import default, Unicode, List
+from tornado import gen
 
 class DockerImageChooserSpawner(DockerSpawner):
-	'''In your JupyterHub configuration:
+    '''Enable the user to select the docker image that gets spawned.
+    
+    Define the available docker images in the JupyterHub configuration and pull
+    them to the execution nodes:
 
-	c.JupyterHub.spawner_class = DockerImageChooserSpawner
-	c.DockerImageChooserSpawner.dockerimages = [
-		'jupyterhub/singleuser',
-		'jupyter/r-singleuser'
-	]
-	'''
-	
-	dockerimages = List(
-		trait = Unicode(),
-		default_value = ['jupyterhub/singleuser'],
-		minlen = 1,
-		config = True,
-		help = "Docker images that have been pre-pulled to the execution host."
-	)
-	form_template = Unicode("""
-		<label for="dockerimage">Select a Docker image:</label>
+    c.JupyterHub.spawner_class = DockerImageChooserSpawner
+    c.DockerImageChooserSpawner.dockerimages = [
+        'jupyterhub/singleuser',
+        'jupyter/r-singleuser'
+    ]
+    '''
+    
+    dockerimages = List(
+        trait = Unicode(),
+        default_value = ['jupyterhub/singleuser'],
+        minlen = 1,
+        config = True,
+        help = "Docker images that have been pre-pulled to the execution host."
+    )
+    form_template = Unicode("""
+        <label for="dockerimage">Select a Docker image:</label>
         <select class="form-control" name="dockerimage" required autofocus>
-			{option_template}
+            {option_template}
         </select>""",
-		config = True, help = "Form template."
-	)
-	option_template = Unicode("""
-		<option value="{image}">{image}</option>""",
-		config = True, help = "Template for html form options."
-	)
-	
-	@default('options_form')
-	def _options_form(self):
-		options = ''.join([
-			self.option_template.format(image=di) for di in self.dockerimages
-		])
-		return self.form_template.format(option_template=options)
+        config = True, help = "Form template."
+    )
+    option_template = Unicode("""
+        <option value="{image}">{image}</option>""",
+        config = True, help = "Template for html form options."
+    )
 
-	def options_from_form(self, formdata):
-		"""formdata looks like {'dockerimage': ['jupyterhub/singleuser']}"""
-		default = self.dockerimages[0]
-		dockerimage = formdata.get('dockerimage', [default])[0]
-		if dockerimage not in self.dockerimages:
-			dockerimage = default
+    @default('options_form')
+    def _options_form(self):
+        """Return the form with the drop-down menu."""
+        options = ''.join([
+            self.option_template.format(image=di) for di in self.dockerimages
+        ])
+        return self.form_template.format(option_template=options)
 
-		# The prefix of the user's container name is normally "jupyter".
-		# Since a single user will launch different containers (though not
-		# simultaneously), they should have different prefixes.
-		# We borrow the image name for the prefix.
-		self.container_prefix = dockerimage.replace('/', '-')
-		self.container_image = dockerimage
+    def options_from_form(self, formdata):
+        """Parse the submitted form data and turn it into the correct
+           structures for self.user_options."""
 
-		#options = { 'container': container }
-		#return options
-		return {}
+        default = self.dockerimages[0]
+
+        # formdata looks like {'dockerimage': ['jupyterhub/singleuser']}"""
+        dockerimage = formdata.get('dockerimage', [default])[0]
+
+        # Don't allow users to input their own images
+        if dockerimage not in self.dockerimages: dockerimage = default
+
+        # container_prefix: The prefix of the user's container name is normally
+        # "jupyter". Since a single user may launch different containers
+        # (even though not simultaneously), they should have different
+        # prefixes. Otherwise docker will only save one container per user. We
+        # borrow the image name for the prefix.
+        options = {
+            'container_image': dockerimage,
+            'container_prefix': '{}-{}'.format(
+                'jupyter', dockerimage.replace('/', '-')
+            )
+        }
+        return options
+
+    @gen.coroutine
+    def start(self, image=None, extra_create_kwargs=None,
+            extra_start_kwargs=None, extra_host_config=None):
+        # container_prefix is used to construct container_name
+        self.container_prefix = self.user_options['container_prefix']
+
+        # start the container
+        yield DockerSpawner.start(
+            self, image=self.user_options['container_image'],
+            extra_create_kwargs=extra_create_kwargs,
+            extra_host_config=extra_host_config)
+    
+# http://jupyter.readthedocs.io/en/latest/development_guide/coding_style.html
+# vim: set ai et ts=4 sw=4:
